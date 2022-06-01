a3e4ca4e8a6c763a22eeeb57a344eecdba2d5886
Ikrom <ikromyusupov09@gmail.com>
2022-06-01 18:47:03 +0500

    Find the fraction of the number

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,5 +1,5 @@
 #Create a variable called 'number' with data type int.
-number = 61
+number = 22
 #Divide the number by 2 and assign number.
 
 #Find the fraction of the number and assing to a variable called 'answer'.
